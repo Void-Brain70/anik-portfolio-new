da9fad2a4b13738a7a42e31f5ea1d1a15be70645
Anik Chandra <dasssanik124102@gmail.com>
2023-10-15 20:48:28 +0600

    photography section

--- src/pages/about-me.vue
+++ src/pages/about-me.vue
@@ -4,16 +4,16 @@
       <div class="bg-[#161616]">
         <div class="container mx-auto my-auto">
           <div class="flex justify-center ">
-            <h3 class="text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500 max-w-fit">
+            <h3 class="font-['Young_Serif' 'serif'] font-[600] text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500 max-w-fit">
               My Job Information
             </h3>
           </div>
           <div class="md:flex md:justify-between items-center  py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Company Name: <span class="text-[500]">Nextive Solution</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Company Name: <span class="text-[500]">Nextive Solution</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> My Position: <span>Web Developer</span></p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Joining Date:
+              <p class="font-['Young_Serif' 'serif'] font-[600]  text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> My Position: <span>Web Developer</span></p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Joining Date:
                 <span>1, December, 2022 - Running</span></p>
             </div>
             <div data-aos="flip-left"
@@ -29,18 +29,18 @@
       <div class="bg-[#161616]">
         <div class="container mx-auto my-auto">
           <div class="flex justify-center">
-            <h3 class="text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] max-w-fit text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500">
+            <h3 class="font-['Young_Serif' 'serif'] font-[600] text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] max-w-fit text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500">
               My University Information</h3>
           </div>
           <div class="md:flex md:justify-between items-center py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" /> University Name: <span class="text-[500]">University of Asia Pacific</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" /> University Name: <span class="text-[500]">University of Asia Pacific</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Department: <span class="text-[500]">Computer Science and Engineering (CSE)</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Department: <span class="text-[500]">Computer Science and Engineering (CSE)</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Session: <span>Spring 2018 - Fall 2021</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Session: <span>Spring 2018 - Fall 2021</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  CGPA: <span>3.74 Out of 4.00</span></p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  CGPA: <span>3.74 Out of 4.00</span></p>
             </div>
             <div data-aos="flip-left"
                  data-aos-easing="ease-out-cubic"
@@ -56,24 +56,50 @@
       <div class="bg-[#161616]">
         <div class="container mx-auto my-auto">
           <div class="flex justify-center">
-            <h3 class="text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] max-w-fit text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500">
+            <h3 class="font-['Young_Serif' 'serif'] font-[600] text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] max-w-fit text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500">
               My College
               Information</h3>
           </div>
           <div class="md:flex md:justify-between items-center py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" />  College Name: <span class="text-[500]">Govt. Science College, Dhaka</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" />  College Name: <span class="text-[500]">Govt. Science College, Dhaka</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Group: <span class="text-[500]">Science</span>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Group: <span class="text-[500]">Science</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Batch: <span>HSC 2016</span></p>
-              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> GPA: <span>4.92 Out of 5.00</span></p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Batch: <span>HSC 2016</span></p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> GPA: <span>4.92 Out of 5.00</span></p>
             </div>
             <div data-aos="flip-left"
                  data-aos-easing="ease-out-cubic"
                  data-aos-duration="2000">
               <div class="flex justify-center md:block drop-shadow-2xl">
                 <img class="h-[120px] md:h-[140px]" src="../public/logo/gsc.png"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#161616]">
+        <div class="container mx-auto my-auto">
+          <div class="flex justify-center">
+            <h3 class="font-['Young_Serif' 'serif'] font-[600] text-center pt-8 text-[24px] border-b-4 border-[#5b6cf1] max-w-fit text-transparent  bg-clip-text  bg-gradient-to-r from-sky-500 to-indigo-500">
+              My School
+              Information</h3>
+          </div>
+          <div class="md:flex md:justify-between items-center py-8">
+            <div class="pb-8 md:pb-0">
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:shamrock" />  School Name: <span class="text-[500]">Dashani Mohanpur High School, Chandpur</span>
+              </p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:shamrock" /> Group: <span class="text-[500]">Science</span>
+              </p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:shamrock" /> Batch: <span>SSC 2014</span></p>
+              <p class="font-['Young_Serif' 'serif'] font-[600] text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:shamrock" /> GPA: <span>5.00 Out of 5.00</span></p>
+            </div>
+            <div data-aos="flip-left"
+                 data-aos-easing="ease-out-cubic"
+                 data-aos-duration="2000">
+              <div class="flex justify-center md:block drop-shadow-2xl">
+                <img class="h-[120px] md:h-[140px]" src="../public/logo/DMHS.png"/>
               </div>
             </div>
           </div>
