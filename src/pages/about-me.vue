730380cabfe33e8c353b19959f00312c62b7e99c
Anik Chandra <dasssanik124102@gmail.com>
2023-10-15 20:15:19 +0600

    photography section

--- src/pages/about-me.vue
+++ src/pages/about-me.vue
@@ -10,10 +10,10 @@
           </div>
           <div class="md:flex md:justify-between items-center  py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]">Company Name: <span class="text-[500]">Nextive Solution</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Company Name: <span class="text-[500]">Nextive Solution</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">My Position: <span>Web Developer</span></p>
-              <p class="text-center text-white md:text-left text-[20px]">Joining Date:
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> My Position: <span>Web Developer</span></p>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:hibiscus"/> Joining Date:
                 <span>1, December, 2022 - Running</span></p>
             </div>
             <div data-aos="flip-left"
@@ -34,13 +34,13 @@
           </div>
           <div class="md:flex md:justify-between items-center py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]">University Name: <span class="text-[500]">University of Asia Pacific</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" /> University Name: <span class="text-[500]">University of Asia Pacific</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">Department: <span class="text-[500]">Computer Science and Engineering (CSE)</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Department: <span class="text-[500]">Computer Science and Engineering (CSE)</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">Session: <span>Spring 2018 - Fall 2021</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  Session: <span>Spring 2018 - Fall 2021</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">CGPA: <span>3.74 Out of 4.00</span></p>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:herb" />  CGPA: <span>3.74 Out of 4.00</span></p>
             </div>
             <div data-aos="flip-left"
                  data-aos-easing="ease-out-cubic"
@@ -62,12 +62,12 @@
           </div>
           <div class="md:flex md:justify-between items-center py-8">
             <div class="pb-8 md:pb-0">
-              <p class="text-center text-white md:text-left text-[20px]">College Name: <span class="text-[500]">Govt. Science College, Dhaka</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" />  College Name: <span class="text-[500]">Govt. Science College, Dhaka</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">Group: <span class="text-[500]">Science</span>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Group: <span class="text-[500]">Science</span>
               </p>
-              <p class="text-center text-white md:text-left text-[20px]">Batch: <span>HSC 2016</span></p>
-              <p class="text-center text-white md:text-left text-[20px]">GPA: <span>4.92 Out of 5.00</span></p>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> Batch: <span>HSC 2016</span></p>
+              <p class="text-center text-white md:text-left text-[20px]"><icon name="fluent-emoji-flat:maple-leaf" /> GPA: <span>4.92 Out of 5.00</span></p>
             </div>
             <div data-aos="flip-left"
                  data-aos-easing="ease-out-cubic"
